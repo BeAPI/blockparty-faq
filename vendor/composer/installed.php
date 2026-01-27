fedd357a71b86a1399ba7d55f6b3e16621c2346d
Milan Ricoul <mricoul@beapi.fr>
2026-01-27 08:25:23 +0000

    Release version 2.0.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'beapi/blockparty-faq',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd52d58b2b27ad6bcc751f408f8e387f1837bed08',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'beapi/blockparty-faq' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd52d58b2b27ad6bcc751f408f8e387f1837bed08',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'composer/installers' => array(
+            'pretty_version' => 'v2.3.0',
+            'version' => '2.3.0.0',
+            'reference' => '12fb2dfe5e16183de69e784a7b84046c43d97e8e',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
