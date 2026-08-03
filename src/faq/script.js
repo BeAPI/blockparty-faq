4b98c0604651457ee3ea7eefefcc5b2d1545e4ce
Paolo Tesei <ptesei@beapi.fr>
2026-08-03 16:22:09 +0200

    perf remove window load

--- src/faq/script.js
+++ src/faq/script.js
@@ -1,17 +1,26 @@
-import { Accordion } from '@beapi/be-a11y';
+import { Accordion } from "@beapi/be-a11y";
 
 // eslint-disable-next-line no-undef
 const accordionConfig = beapiFaqBlock.accordionConfig;
 
-window.addEventListener( 'load', function () {
+/**
+ * Initialize FAQ accordion instances.
+ */
+const init = () => {
 	Accordion.init(
-		'.wp-block-blockparty-faq:has(button.wp-block-blockparty-faq-trigger)',
-		accordionConfig
+		".wp-block-blockparty-faq:has(button.wp-block-blockparty-faq-trigger)",
+		accordionConfig,
 	);
 
-	Accordion.init( '.wp-block-blockparty-faq:has(button.faq__trigger)', {
+	Accordion.init(".wp-block-blockparty-faq:has(button.faq__trigger)", {
 		...accordionConfig,
-		panelSelector: '.faq__panel',
-		triggerSelector: '.faq__trigger',
-	} );
-} );
+		panelSelector: ".faq__panel",
+		triggerSelector: ".faq__trigger",
+	});
+};
+
+if (document.readyState === "loading") {
+	document.addEventListener("DOMContentLoaded", init);
+} else {
+	init();
+}
