0501ea4a2eef6e851fb6591ad148fe67c25522cb
Paolo Tesei <ptesei@beapi.fr>
2026-08-04 09:26:36 +0200

    fix format script

--- src/faq/script.js
+++ src/faq/script.js
@@ -1,4 +1,4 @@
-import { Accordion } from "@beapi/be-a11y";
+import { Accordion } from '@beapi/be-a11y';
 
 // eslint-disable-next-line no-undef
 const accordionConfig = beapiFaqBlock.accordionConfig;
@@ -8,19 +8,19 @@ const accordionConfig = beapiFaqBlock.accordionConfig;
  */
 const init = () => {
 	Accordion.init(
-		".wp-block-blockparty-faq:has(button.wp-block-blockparty-faq-trigger)",
-		accordionConfig,
+		'.wp-block-blockparty-faq:has(button.wp-block-blockparty-faq-trigger)',
+		accordionConfig
 	);
 
-	Accordion.init(".wp-block-blockparty-faq:has(button.faq__trigger)", {
+	Accordion.init( '.wp-block-blockparty-faq:has(button.faq__trigger)', {
 		...accordionConfig,
-		panelSelector: ".faq__panel",
-		triggerSelector: ".faq__trigger",
-	});
+		panelSelector: '.faq__panel',
+		triggerSelector: '.faq__trigger',
+	} );
 };
 
-if (document.readyState === "loading") {
-	document.addEventListener("DOMContentLoaded", init);
+if ( document.readyState === 'loading' ) {
+	document.addEventListener( 'DOMContentLoaded', init );
 } else {
 	init();
 }
