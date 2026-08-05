74a7a7cd799a76d77931d530dc36614bb473cf35
Paolo Tesei <ptesei@beapi.fr>
2026-08-05 15:40:32 +0000

    Release version 2.1.1

---
+++ build/faq-answer/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n', 'wp-primitives'), 'version' => '01be3a4dddbca783ee84');
---
+++ build/faq-question/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b0e0f9f8afd680ba5df0');
